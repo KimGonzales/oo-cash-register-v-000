9c9da9abdfc23f1402f668930a0de633625555a3
Kim Gonzales <kimgo589@gmail.com>
2018-02-06 17:03:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
 
   def apply_discount
     if discount > 0
-      total_after_discount = @total - (@total * (discount/100))
+      @discount = (discount/100) * total
     end
   end
 
